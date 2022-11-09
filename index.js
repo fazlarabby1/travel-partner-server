11ce06378e527a54b3a753d3d28e1b7cd8e7de6b
fazlarabby1 <fazlarabby41@gmail.com>
2022-11-09 18:54:09 +0600

    created review api to store review in the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 require('dotenv').config();
@@ -19,14 +19,40 @@ async function run() {
     try {
         // service collection
         const serviceCollection = client.db('travelPartner').collection('services');
+        // Review collection
+        const reviewCollection = client.db('travelPartner').collection('reviews');
 
-        // getting 3 services data
+        // api for 3 services data
         app.get('/services3', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
         });
+
+        // api for all the services
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // api for a single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // review api
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            console.log(review)
+            res.send(result);
+        })
     }
     finally {
 
